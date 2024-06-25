57271e38f638cee0bdf20805842c9c1448c6b602
Ricardo da Hora Silva (Admin) <ue_e-rsilva@tapnet.tap.pt>
2024-06-25 18:12:42 +0100

    Create New Resource

--- main.tf
+++ main.tf
@@ -28,8 +28,11 @@ resource "azurerm_resource_group" "example" {
 # Criação do SQL Server
 resource "azurerm_mssql_server" "example" {
   name                         = var.sql_server_name
-  resource_group_name          = data.azurerm_resource_group.existing_rg.name
-  location                     = data.azurerm_resource_group.existing_rg.location
+  # resource_group_name          = data.azurerm_resource_group.existing_rg.name
+  # location                     = data.azurerm_resource_group.existing_rg.location
+  resource_group_name           = azurerm_resource_group.example.name
+  location                      = azurerm_resource_group.example.location
+
   version                      = "12.0"
   administrator_login          = var.admin_username
   administrator_login_password = var.admin_password
